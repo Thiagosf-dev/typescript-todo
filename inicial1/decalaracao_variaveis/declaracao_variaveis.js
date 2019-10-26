607569237ba3bf232ad180273e06b69b0e368981
Thiago Soares Figueiredo <thiagosf.dev@gmail.com>
2019-10-26 09:13:33 -0300

    Declaração de variáveis

---
+++ inicial1/decalaracao_variaveis/declaracao_variaveis.js
@@ -0,0 +1,8 @@
+/**
+ * @description Formas de declaração de variável com e sem tipagem
+ * @author Thiago Soares Figueiredo
+ *
+ */
+var string1; // declaração sem inicialização
+var string2 = "Texto"; // declaração com inicialização
+var string3 = "Texto"; // declaração com inicialização sem tipagem especificada (assume o tipo referente ao valor atribuido .:. não aconselhável)
---
+++ inicial1/decalaracao_variaveis/declaracao_variaveis.ts
@@ -0,0 +1,8 @@
+/**
+ * @description Formas de declaração de variável com e sem tipagem
+ * @author Thiago Soares Figueiredo
+ * 
+ */
+var string1: string;            // declaração sem inicialização
+var string2: string = "Texto";  // declaração com inicialização
+var string3 = "Texto";          // declaração com inicialização sem tipagem especificada (assume o tipo referente ao valor atribuido .:. não aconselhável)
